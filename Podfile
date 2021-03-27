c2179bdb7ab9a8fc7eb395d8b44a9759abc33a0c
김동우 <dongwoo@gimdong-uui-MacBookPro.local>
2021-03-27 23:39:21 +0900

    [Add] .gitignore 추가

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Yasty_ios' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'NMapsMap'
+  pod 'Then'
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  # Pods for Yasty_ios
+
+end
---
+++ Yasty_ios/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Yasty_ios
+//
+//  Created by 김동우 on 2021/03/25.
+//
+
+import UIKit
+import Then
+import NMapsMap
+
+
+class ViewController: UIViewController {
+    
+    let mapView = NMFMapView()
+    let searchTextField = UITextField()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //mapView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        view.addSubview(mapView)
+        mapView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        
+        
+        
+    }
+
+
+}
+
+extension ViewController: NMFMapViewCameraDelegate {
+    
+}
+
