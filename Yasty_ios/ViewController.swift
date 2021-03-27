b27bbbd903542c57970aff10f27bbf594c0cc277
ornwoo96 <dongou705@naver.com>
2021-03-27 23:52:08 +0900

    sad

--- Yasty_ios/ViewController.swift
+++ Yasty_ios/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     let searchTextField = UITextField()
     
     
-    //
+    ////
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
