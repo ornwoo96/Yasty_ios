7c0687dc27880ea1ee523dec38ce51153e70498d
jeongjaein <wodls96@gmail.com>
2021-05-14 00:26:45 +0900

    feat : bottomsheet 미완성

--- Yasty_ios/Controller/ViewController.swift
+++ Yasty_ios/Controller/ViewController.swift
@@ -17,29 +17,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     //위치 매니저 변수
     var locationManager: CLLocationManager!
     var delegate: HomeControllerDelegate?
-    
-    
-    //let mapView2 = NMFMapView()
     let searchTextField = UITextField()
     let paddingView = UIView()
     let mapView = NMFNaverMapView()
     let mapView2 = NMFMapView()
     let sideButton = UIButton()
     let gifIamgeView = GIFImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-    
-    //위도와 경도
+    let bottomTempButton = UIButton()
+    let bottomSheet = UIView()
+    let indicatorBar = UIView()
+    let indicatorBackgroundView = UIView()
+    let screenSize = UIScreen.main.bounds
     var latitude: Double?
     var longitude: Double?
+    lazy var bottomPangesture = UIPanGestureRecognizer(
+        target: self, action: #selector(bottomPangestureDidStart))
+    var preYValue: CGFloat = 0.0
     
-    //
     override func viewDidLoad() {
         super.viewDidLoad()
-//        configureNavigationBar()
         attribute()
         layout()
         myLocation()
-        
-        
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization() // when in use auth로 요청 등록
@@ -47,9 +46,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let coor = locationManager.location?.coordinate
         latitude = coor?.latitude
         longitude = coor?.longitude
-                
     }
-    
     
     // state
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -86,10 +83,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             //라운드
             $0.layer.cornerRadius = 10
             $0.layer.masksToBounds = true
-            
-            //테두리 (음영처리는 추가적으로)
-//            $0.layer.borderWidth = 0.5
-//            $0.layer.borderColor = UIColor.gray.cgColor
             $0.placeholder = "맛집 키워드 검색"
             $0.layer.shadowColor = UIColor.gray.cgColor
             $0.layer.masksToBounds = false
@@ -107,40 +100,104 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             $0.setImage(UIImage(systemName: "line.horizontal.3"), for: .normal)
             $0.addTarget(self, action: #selector(handleMenuToggle), for: .touchUpInside)
         }
+        bottomTempButton.do {
+            $0.setTitle("바텀", for: .normal)
+            $0.backgroundColor = .black
+            $0.addTarget(self, action: #selector(tempButtonDidTap), for: .touchUpInside)
+        }
+        indicatorBar.do {
+            $0.backgroundColor = .gray
+            $0.alpha = 0.1
+            $0.layer.cornerRadius = 3
+            $0.layer.masksToBounds = true
+        }
+        indicatorBackgroundView.do {
+            $0.backgroundColor = .clear
+            $0.addGestureRecognizer(bottomPangesture)
+        }
+        bottomSheet.do {
+            $0.layer.cornerRadius = 25
+            $0.layer.masksToBounds = true
+            $0.backgroundColor = .white
+            $0.frame = CGRect(x: 0,
+                              y: screenSize.height,
+                              width: screenSize.width,
+                              height: screenSize.height / 3)
+        }
     }
+    
     // MARK: - Handlers
+    
+    @objc func bottomPangestureDidStart(_ gesture: UIPanGestureRecognizer) {
+        
+        let gestureY = gesture.translation(in: bottomSheet).y
+        let calculatedY = preYValue - gestureY
+        preYValue = gestureY
+        if gesture.state == .changed {
+            if bottomSheet.frame.minY >= (screenSize.height - screenSize.height / 3) {
+                var finalCalculatedY = bottomSheet.frame.minY - calculatedY
+                if finalCalculatedY < (screenSize.height - screenSize.height / 3) {
+                    finalCalculatedY = screenSize.height - screenSize.height / 3
+                }
+                bottomSheet.frame = CGRect(x: 0,
+                                           y: finalCalculatedY,
+                                           width: screenSize.width,
+                                           height: screenSize.height / 3)
+            }
+        } else if gesture.state == .ended {
+            if gesture.velocity(in: bottomSheet).y < 0 {
+                UIView.animate(withDuration: 0.1) { [weak self] in
+                    self?.bottomSheet.frame = CGRect(x: 0,
+                                                     y: self!.screenSize.height
+                                                        - self!.screenSize.height / 3,
+                                                     width: self!.screenSize.width,
+                                                     height: self!.screenSize.height / 3)
+                }
+            } else {
+                UIView.animate(withDuration: 0.15) { [weak self] in
+                    self?.bottomSheet.frame = CGRect(x: 0,
+                                                     y: self!.screenSize.height,
+                                                     width: self!.screenSize.width,
+                                                     height: self!.screenSize.height / 3)
+                }
+            }
+        }
+    }
+    
+    @objc func tempButtonDidTap() {
+        UIView.animate(withDuration: 0.25) { [weak self] in
+            self?.bottomSheet.frame = CGRect(x: 0,
+                                             y: self!.screenSize.height
+                                                - self!.screenSize.height / 3,
+                                             width: self!.screenSize.width,
+                                             height: self!.screenSize.height / 3)
+        }
+    }
     
     @objc func handleMenuToggle() {
         delegate?.handleMenuToggle()
     }
     
     func layout() {
-        [ mapView, searchTextField , sideButton ].forEach {
+        [ mapView, searchTextField , sideButton, bottomTempButton, bottomSheet ].forEach {
             view.addSubview($0)
         }
+        bottomSheet.addSubview(indicatorBar)
+        bottomSheet.addSubview(indicatorBackgroundView)
         
         mapView.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            //위
             $0.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-            //밑
             $0.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-            //왼쪽
             $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-            //오른쪽
             $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         }
         
         searchTextField.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            //위
-            //세이프에이리어 개념
             $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-            //왼쪽
             $0.leadingAnchor.constraint(equalTo: sideButton.trailingAnchor, constant: 10).isActive = true
-            //오른쪽
             $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-            //높이
             $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
         }
         
@@ -150,12 +207,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
             $0.heightAnchor.constraint(equalToConstant: 40).isActive = true
             $0.widthAnchor.constraint(equalToConstant: 40).isActive = true
-            
-            
         }
-        
+        bottomTempButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+            $0.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: 10).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        }
+        indicatorBar.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: bottomSheet.topAnchor, constant: 10).isActive = true
+            $0.centerXAnchor.constraint(equalTo: bottomSheet.centerXAnchor).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 10).isActive = true
+        }
+        indicatorBackgroundView.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: bottomSheet.topAnchor).isActive = true
+            $0.centerXAnchor.constraint(equalTo: bottomSheet.centerXAnchor).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 100).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        }
     }
-    
 }
 
 extension ViewController: UITextFieldDelegate {
